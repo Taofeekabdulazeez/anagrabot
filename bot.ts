426a8521a2cd6d33cb693f21b8ca0c318b0c4bbc
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2025-07-20 00:16:46 +0100

    update bot to use WELCOME_MESSAGE constant and change dotenv config path to .env.local

--- bot.ts
+++ bot.ts
@@ -1,13 +1,14 @@
 import { Bot } from "grammy";
 import * as dotenv from "dotenv";
+import { WELCOME_MESSAGE } from "./constants";
 
-dotenv.config({ path: ".env" });
+dotenv.config({ path: ".env.local" });
 
-const BOT_TOKEN = String(process.env.BOT_TOKEN);
+const BOT_TOKEN = process.env.BOT_TOKEN!;
 
 const bot = new Bot(BOT_TOKEN);
 
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+bot.command("start", (ctx) => ctx.reply(WELCOME_MESSAGE));
 
 bot.on("message", (ctx) =>
   ctx.reply(`Received your message! ${ctx.message.text}`)
