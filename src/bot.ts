0c71d9d0c705b5e23eeba5eb27ff275be064cf7e
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2025-07-20 08:45:09 +0100

    refactor: improve message handling by adding error handling and chat action feedback

--- src/bot.ts
+++ src/bot.ts
@@ -11,24 +11,35 @@ const bot = new Bot(BOT_TOKEN);
 
 bot.command("start", (ctx) => ctx.reply(WELCOME_MESSAGE));
 
-bot.on("message", async (ctx) => {
-  if (ctx.message.text?.length! > 10) {
-    ctx.reply("Word too long: please make sure the length doesn't exceed 10.");
-    return;
+bot.on("message:text", async (ctx) => {
+  try {
+    await bot.api.sendChatAction(ctx.chat.id, "typing");
+
+    if (ctx.message.text?.length! > 10) {
+      ctx.reply(
+        "Word too long: please make sure the length doesn't exceed 10."
+      );
+      return;
+    }
+
+    const response = await axios.get(
+      `https://anagram-solver.onrender.com/anagrams/${ctx.message.text?.toLowerCase()}`
+    );
+
+    if (response.status !== 200) {
+      ctx.reply("Error fetching anagrams. Please try again later.");
+      return;
+    }
+
+    const { data, results, word } = response.data;
+
+    ctx.reply(`Found ${results} results for "${word}":\n${data.join("\n")}`);
+  } catch (error) {
+    console.log(error);
+    ctx.reply(
+      "An error occurred while processing your request. Please try again."
+    );
   }
-
-  const response = await axios.get(
-    `https://anagram-solver.onrender.com/anagrams/${ctx.message.text?.toLowerCase()}`
-  );
-
-  if (response.status !== 200) {
-    ctx.reply("Error fetching anagrams. Please try again later.");
-    return;
-  }
-
-  const { data, results, word } = response.data;
-
-  ctx.reply(`Found ${results} results for "${word}":\n${data.join("\n")}`);
 });
 
 bot.start();
