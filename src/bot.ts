bb18a313ca86649540139d0dad5415ee887532f1
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2025-07-20 01:51:40 +0100

    refactor: rewrite bot functionality and reintroduce WELCOME_MESSAGE constant

---
+++ src/bot.ts
@@ -0,0 +1,34 @@
+import { Bot } from "grammy";
+import * as dotenv from "dotenv";
+import { WELCOME_MESSAGE } from "./constants";
+import axios from "axios";
+
+dotenv.config({ path: ".env.local" });
+
+const BOT_TOKEN = process.env.BOT_TOKEN!;
+
+const bot = new Bot(BOT_TOKEN);
+
+bot.command("start", (ctx) => ctx.reply(WELCOME_MESSAGE));
+
+bot.on("message", async (ctx) => {
+  if (ctx.message.text?.length! > 10) {
+    ctx.reply("Word too long: please make sure the length doesn't exceed 10.");
+    return;
+  }
+
+  const response = await axios.get(
+    `https://anagram-solver.onrender.com/anagrams/${ctx.message.text?.toLowerCase()}`
+  );
+
+  if (response.status !== 200) {
+    ctx.reply("Error fetching anagrams. Please try again later.");
+    return;
+  }
+
+  const { data, results, word } = response.data;
+
+  ctx.reply(`Found ${results} results for "${word}":\n${data.join("\n")}`);
+});
+
+bot.start();
